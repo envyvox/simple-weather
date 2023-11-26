9523026fa82ba910e15af47dd937040a8cc48672
envyvox <envyvox@gmail.com>
2023-11-26 16:29:32 +0200

    add cors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,26 @@ const nextConfig = {
     WEATHER_API_URL: process.env.WEATHER_API_URL,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
