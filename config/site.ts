deef9ee496097d517451d1f95f7f5e5978377963
envyvox <envyvox@gmail.com>
2023-11-27 17:39:17 +0200

    added favicon and desc

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,8 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Simple Weather",
-  description: "Simple weather website.",
+  description:
+    "Explore the weather forecast for Ukrainian cities over the next 3 days. Simple and user-friendly website, without unnecessary buttons, ads, and distractions.",
   links: {
     twitter: "https://twitter.com/envyvox",
     github: "https://github.com/envyvox/simple-weather",
