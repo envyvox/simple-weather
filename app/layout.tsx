1a203ba0df66cb086f1d927824d216d6aa9a70d2
envyvox <envyvox@gmail.com>
2023-11-28 11:20:43 +0200

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           )}
         >
           <div className="relative flex flex-col">
-            <div className="flex-1 min-h-[93vh]">{children}</div>
+            <div className="flex-1">{children}</div>
             <Footer />
           </div>
           <TailwindIndicator />
